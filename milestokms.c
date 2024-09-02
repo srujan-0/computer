f89201bc6d4f0f56579a600e159437e30d827027
Srujan <130295979+srujan-0@users.noreply.github.com>
2024-09-02 09:16:38 +0530

    Add files via upload

---
+++ milestokms.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+    float miles,kms;
+    printf("enter distance in miles\n");
+    scanf("%f",&miles);
+    kms=1.61*miles;
+    printf("distance in kms=%.2f",kms);
+    
+
+
+
+
+}
